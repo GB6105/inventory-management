3d62e009882ac351af5779beab981ac56707ec9b
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-11-11 14:46:38 +0900

    feat(controller) : 기본 API 엔드 포인트 구현

--- src/main/java/gb6105/inventory/Controller/StockController.java
+++ src/main/java/gb6105/inventory/Controller/StockController.java
@@ -1,4 +1,29 @@
 package gb6105.inventory.Controller;
 
+import gb6105.inventory.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/stocks")
 public class StockController {
+    private final StockService stockService;
+
+    @PostMapping({"/stockId}/decrease"})
+    public ResponseEntity<Void> decreaseStock(@PathVariable Long stockId, @RequestParam Long quantity) {
+        try{
+            stockService.decreaseStock(stockId, quantity);
+            return ResponseEntity.ok().build();
+        }catch(IllegalArgumentException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
 }
