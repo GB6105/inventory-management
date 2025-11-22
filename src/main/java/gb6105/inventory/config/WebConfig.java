927dfc9770671070eb02266e180b1c91817d2b53
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-11-23 02:53:54 +0900

    fix(config) : cors 주소 추가

--- src/main/java/gb6105/inventory/config/WebConfig.java
+++ src/main/java/gb6105/inventory/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    private static final String AWS_IP_ADDRESS = "3.38.114.206"
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -14,7 +15,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS를 적용
-                        .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173") // 허용할 출처
+                        .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173",
+                                "http://" + AWS_IP_ADDRESS + ":5173") // 허용할 출처
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메서드
                         .allowedHeaders("*") // 모든 헤더를 허용
                         .allowCredentials(true) // 자격 증명을 허용
